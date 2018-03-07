3fb420e2317be8037ad064f891e607a6a10334d2
Victor <tolmachev.2406@gmail.com>
2018-03-07 19:07:49 +0300

    Add adjacency_list.csv

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from lxml import etree
 from time import time
 import svgwrite
-
+import csv
 
 def convertCoordinate(coordinate):
     return coordinate * 1500
 
 
-def main():
+def parse(file):
     bounds = {}
     nodes = {}  # {'id': {'lat': str, 'lon': str, used: bool}}
     ways = []  #
 
-    for _, element in etree.iterparse('../../osmosis/krasnodar.osm', tag=['bounds', 'node', 'way']):
+    for _, element in etree.iterparse(file, tag=['bounds', 'node', 'way']):
         if element.tag == 'bounds':
             bounds['minlat'] = element.get('minlat')
             bounds['minlon'] = element.get('minlon')
@@ -21,26 +21,68 @@ def main():
         elif element.tag == 'node':
             nodes[element.get('id')] = {'lat': element.get('lat'), 'lon': element.get('lon'), 'used': False}
         elif element.tag == 'way':
-            rfnodes = []
+            lst_nodesID = []
             for child in element.iter('nd', 'tag'):
                 if child.tag == 'nd':
-                    rfnodes.append(child.get('ref'))
+                    lst_nodesID.append(child.get('ref'))
                 elif child.tag == 'tag' and child.get('k') == 'highway':
-                    ways.append(rfnodes)
+                    ways.append(lst_nodesID)
         element.clear()
+
+    return bounds, nodes, ways
+
+
+def main():
+
+    bounds, nodes, ways = parse('../../osmosis/krasnodar.osm')
+    adj_list = {}
+
 
     svg_document = svgwrite.Drawing(filename='map.svg')
 
+    i = 0
     for way in ways:
-        nodes_print = []
+        printed_nodes = []
         for nodeID in way:
             if nodeID in nodes:
                 nodes[nodeID]['used'] = True
-                nodes_print.append((convertCoordinate(float(nodes[nodeID]['lon']) - float(bounds['minlon'])),
-                              convertCoordinate(float(nodes[nodeID]['lat']) - float(bounds['minlat']))))
-        svg_document.add(svgwrite.shapes.Polyline(nodes_print, fill='none', stroke='black'))
+                printed_nodes.append((convertCoordinate(float(nodes[nodeID]['lon']) - float(bounds['minlon'])),
+                                      convertCoordinate(float(nodes[nodeID]['lat']) - float(bounds['minlat']))))
+
+        svg_document.add(svgwrite.shapes.Polyline(printed_nodes, fill='none', stroke='black', stroke_width=0.5))
+
+    for nodeID in nodes:
+        if nodes[nodeID]['used']:
+            svg_document.add(svgwrite.shapes.Circle(
+                center=(convertCoordinate(float(nodes[nodeID]['lon']) - float(bounds['minlon'])),
+                        convertCoordinate(float(nodes[nodeID]['lat']) - float(bounds['minlat']))),
+                r=0.2, fill='red'))
 
     svg_document.save()
+
+
+    for way in ways:
+        if adj_list.get(way[0]):
+            adj_list[way[0]].append(way[1])
+        else:
+            adj_list[way[0]] = [way[1]]
+        i = 1
+        while i < len(way) - 1:
+            if adj_list.get(way[i]):
+                adj_list[way[i]].extend([way[i-1], way[i+1]])
+            else:
+                adj_list[way[i]] = [way[i-1], way[i+1]]
+            i = i + 1
+        if adj_list.get(way[i]):
+            adj_list[way[i]].append(way[i-1])
+        else:
+            adj_list[way[i]] = [way[i-1]]
+
+    with open('adjacency_list.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        for ID in adj_list:
+            writer.writerow([ID] + adj_list[ID])
+
 
 
 if __name__ == "__main__":
